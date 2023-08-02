f1dad7f83498ada437ccf3a9fa05e23aac04656d
Rizwanul Hassan <rizwanulhassan2020@gmail.com>
2023-08-02 10:49:17 +0600

    md5 and bcrypt hashing tech is used

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const express=require("express");
 const ejs=require("ejs");
 const app=express();
-const md5=require("md5");
+// const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const mongoose=require("mongoose");
 // const encrypt=require("mongoose-encryption");
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
@@ -36,24 +38,32 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-      const user=new User({
-            email:req.body.username,
-            password:md5(req.body.password)
-      });
-      user.save();
-      res.render("home"); 
+      bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+            // Store hash in your password DB.
+            const user=new User({
+                  email:req.body.username,
+                  password:hash
+            });
+            user.save();
+            res.render("home"); 
+        });
+        
+      
 })
 app.post("/login",function(req,res){
       const username=req.body.username;
-      const password=md5(req.body.password);
+      const password=req.body.password;
       User.findOne({email:username}).then(function(data){
             if(data){
-                  if(data.password===password){
-                        res.render("secrets");
-                  }
-                  else{
-                        res.send("pass didn't match");
-                  }
+                  bcrypt.compare(password, data.password, function(err, result) {
+                        if(result == true){
+                              res.render("secrets");
+                        }
+                        else{
+                              res.send("pass didn't match");
+                        }
+                        
+                    });
             }
             else{
                   res.send("email didn't match");
